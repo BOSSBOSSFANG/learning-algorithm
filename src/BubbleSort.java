cd63d74510ec1c5cac19d639c85621ac6fe5d6fe
BOSSBOSSFANG <2276741513@qq.com>
2021-08-02 23:16:41 +0800

    insertion sort and compare sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,9 @@
+public class BubbleSort extends SortExample{
+    protected void sort(Comparable[] a){
+
+    }
+    public static void main(String[] args){
+        BubbleSort bs = new BubbleSort();
+        bs.test();
+    }
+}
